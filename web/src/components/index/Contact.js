af0e34110c0e2897cd89c913e252ac46569525da
Eddie Cleary <eddie@eddiecleary.com>
2020-10-25 10:24:50 +0800

    added interactive form styling

--- web/src/components/index/Contact.js
+++ web/src/components/index/Contact.js
@@ -1,10 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import styled from 'styled-components';
 import { navigate } from 'gatsby';
 import Socialicons from '../Socialicons';
 
 export default function Contact() {
-  const [formInfo, setFormInfo] = useState({});
+  const [formInfo, setFormInfo] = useState();
+  const [nameFocus, setNameFocus] = useState(false);
+  const [emailFocus, setEmailFocus] = useState(false);
+  const [messageFocus, setMessageFocus] = useState(false);
+
+  const nameLabelRef = useRef();
+  const emailLabelRef = useRef();
+  const messageLabelRef = useRef();
 
   function encode(data) {
     return Object.keys(data)
@@ -16,6 +23,18 @@ export default function Contact() {
 
   const handleChange = (e) => {
     setFormInfo({ ...formInfo, [e.target.name]: e.target.value });
+  };
+
+  const nameFocusToggle = () => {
+    setNameFocus(!nameFocus);
+  };
+
+  const emailFocusToggle = () => {
+    setEmailFocus(!emailFocus);
+  };
+
+  const messageFocusToggle = () => {
+    setMessageFocus(!messageFocus);
   };
 
   const handleSubmit = (e) => {
@@ -60,18 +79,44 @@ export default function Contact() {
             </label>
           </p>
           <div className="flexWrap">
-            <label className="half" htmlFor="name">
+            <label
+              className={`half ${nameFocus ? 'focused' : ''}`}
+              ref={nameLabelRef}
+              htmlFor="name"
+            >
               Name:
-              <input type="text" name="name" onChange={handleChange} />
+              <input
+                type="text"
+                name="name"
+                onFocus={nameFocusToggle}
+                onBlur={nameFocusToggle}
+                onChange={handleChange}
+              />
             </label>
-            <label className="half" htmlFor="email">
+            <label
+              className={`half ${emailFocus ? 'focused' : ''}`}
+              ref={emailLabelRef}
+              htmlFor="email"
+            >
               Email:
-              <input type="email" name="email" onChange={handleChange} />
+              <input
+                onFocus={emailFocusToggle}
+                onBlur={emailFocusToggle}
+                type="email"
+                name="email"
+                onChange={handleChange}
+              />
             </label>
           </div>
-          <label className="full" htmlFor="message">
+          <label
+            className={`full ${messageFocus ? 'focused' : ''}`}
+            ref={messageLabelRef}
+            htmlFor="message"
+          >
             Message:
             <textarea
+              onFocus={messageFocusToggle}
+              onBlur={messageFocusToggle}
               name="message"
               cols="30"
               rows="12"
@@ -113,6 +158,11 @@ const ContactStyles = styled.section`
       max-width: var(--text-width);
       margin-left: auto;
       margin-right: auto;
+
+      .focused {
+        color: var(--color-accent);
+      }
+
       label {
         text-align: left;
         display: flex;
@@ -138,6 +188,11 @@ const ContactStyles = styled.section`
           border-radius: 0.6rem;
           padding: 0.8rem;
           color: var(--color-light);
+
+          &:focus {
+            border-color: var(--color-accent);
+            outline: none;
+          }
         }
       }
       button {
